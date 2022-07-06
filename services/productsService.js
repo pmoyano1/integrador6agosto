58da18d14930c3018d4149fc21702c9c9339291a
digitalhouse <digitalhouse@MacBook-Pro-de-digitalhouse.local>
2022-07-06 18:18:46 -0300

    service erased

--- services/productsService.js
+++ services/productsService.js
@@ -1,22 +1,3 @@
-const restclient = require('nordic/restclient')({
-    timeout: 10000,
-})
-
-class ProductsService {
-    static getProducts(siteId, name, limit){
-        return restclient.get(`/sites/${siteId}/search`, {
-            params:{
-                q: name,
-                limit
-            }
-            })
-            .then(response => {
-                return response.data.results;
-            })
-            .catch(err => {
-                throw new Error(err)
-            })
-        }
-}
-
-module.exports = ProductsService;
+/**
+ * En este archivo deberás crear el servicio:
+ */
--- tests/unit/productsService.spec.js
+++ tests/unit/productsService.spec.js
@@ -11,20 +11,17 @@ jest.mock('nordic/restclient', () => () => ({
 }));
 
 describe('producsService', () => {
-    it('1) Devuelve un array de productos', async () => {
-        try {
-            const response = await productsService.getProducts('MLA', 'celular', 10);
-            expect(response).toBeInstanceOf(Array);
-        } catch(err) {
-            console.log(err);
-        }
+    it('1) Devuelve un array de productos', () => {
+        return productsService.getProducts('MLA', 'celular', 10)
+            .then(response=> {
+                expect(response).toBeInstanceOf(Array);
+            });
     });
 
-    it('2) Arroja un error cuando falla la llamada', async () => {
-        try {
-            const response = await productsService.getProducts('celular', 10);
-        } catch(err) {
-            expect(err).toBeInstanceOf(Error);
-        }
+    it('2) Arroja un error cuando falla la llamada', () => {
+        return productsService.getProducts('celular', 10)
+            .catch(err => {
+                expect(err).toBeInstanceOf(Error)
+            })
     })
 });
