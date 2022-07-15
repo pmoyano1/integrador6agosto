885352bb84fdc051cde527e8a64ef0ed3c89f039
digitalhouse <digitalhouse@MacBook-Pro-de-digitalhouse.local>
2022-07-15 19:32:41 -0300

    minor changes

--- api/1-products/products.js
+++ api/1-products/products.js
@@ -1,21 +1,21 @@
 const router = require('nordic/ragnar').router();
 /**
-Vamos a “simular” una llamada a la API de MeLi (por el momento, no vamos a interactuar 
-con la API oficial, eso vamos a hacerlo en las próximas clases). Para lograrlo, vamos a 
-necesitar crear un array de productos en el archivo ubicado en /utils/mockProducts.js 
-
-Queremos que al ingresar a la ruta “/api/products” se nos muestren 4 productos distintos en pantalla.
-Los mismos deberán tener las siguientes propiedades (respetar los nombres):
--id
--title
--price
--description
--quantity
-
-Debemos requerir el archivo con los productos para enviarlos como respuesta.
-
-Para que este endpoint funcione, debemos implementar la ruta en /api/index.js
-* */
+ * Vamos a “simular” una llamada a la API de MeLi (por el momento, no vamos a interactuar 
+ * con la API oficial, eso vamos a hacerlo en las próximas clases). Para lograrlo, vamos a 
+ * necesitar crear un array de productos en el archivo ubicado en /utils/mockProducts.js 
+ * 
+ * Queremos que al ingresar a la ruta “/api/products” se nos muestren 4 productos distintos en pantalla.
+ * Los mismos deberán tener las siguientes propiedades (respetar los nombres):
+ * -id
+ * -title
+ * -price
+ * -description
+ * -quantity
+ * 
+ * Debemos requerir el archivo con los productos para enviarlos como respuesta.
+ * 
+ * Para que este endpoint funcione, debemos implementar la ruta en /api/index.js
+ */
 
 router.get('/', (req, res) => {
 
--- utils/mockProducts.js
+++ utils/mockProducts.js
@@ -1,9 +1,9 @@
 /**
  * Crear un array de productos y exportarlo para 
- * que lo utilice la ruta /products de la carpeta api.
+ * que lo utilice la ruta /products de la carpeta /api.
  * 
  * Una vez creado, deberás ejecutar en consola el comando
- * npm run test:unit:watch mockProducts.spec.js y corroborar que
+ * `npm run test:unit:watch mockProducts.spec.js` y corroborar que
  * todos los tests estén pasando.
  */
 
