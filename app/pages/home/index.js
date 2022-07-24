b216ad1304726eccb6ab1404295b9899c2f13b26
andyfrankmeli <ext_anfrank@mercadolibre.com>
2022-07-24 00:13:38 -0300

    add comments for ej-int

---
+++ app/pages/home/index.js
@@ -0,0 +1,30 @@
+/**
+ * En esta carpeta deberás crear la page para la ruta /home, 
+ * con sus archivos respectivos. 
+ * 
+ **************************************************************************
+ * Una vez implementada, ejecutar el comando `npm run test:unit:watch view`
+ * y corroborar que los tests pasan (ignorar los tests skipppeados).
+ **************************************************************************
+ */
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Ejercitación Integradora:
+ * 
+ * Una vez cumplidas todas las consignas, deberás descomentar el segundo
+ * `describe` del archivo de test, quitándole la `x` que le antecede, y 
+ * corroborar que los tests pasan. 
+ * 
+ * NOTA: Para que los tests de la view de esta page pasen, primero deberás
+ * haber implementado el servicio que trae los productos de la API de MeLi.
+ */
