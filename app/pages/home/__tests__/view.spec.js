1ad0ffc3e1186dc521e86a35bd907a49afdffd28
andyfrankmeli <ext_anfrank@mercadolibre.com>
2022-07-27 20:30:29 -0300

    completed view tests

--- app/pages/home/__tests__/view.spec.js
+++ app/pages/home/__tests__/view.spec.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const HomeView = require('../view');
 const { render, screen } = require('@testing-library/react');
-const mockProducts = require('../../../../utils/mockProductsEI');
+const mockProducts = require('../../../../tests/utils/products.json');
 
 describe('Ejercicio 4 - La View de Home', () => {
     let component;
@@ -38,6 +38,7 @@ xdescribe('Ejercitación Integradora - La View de Home', () => {
 
     it('2) Renderiza una lista de productos', () => {
         const products = screen.getAllByRole('listitem');
+        expect(products[0]).toHaveTextContent(/motorola/i);
         expect(products.length).toBe(10);
     });
 });
