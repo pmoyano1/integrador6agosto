2e181f1e8768a4c5d5fc3d67370586b843d50066
andyfrankmeli <ext_anfrank@mercadolibre.com>
2022-07-29 01:42:13 -0300

    currencies test correction

--- tests/unit/currencies-route.spec.js
+++ tests/unit/currencies-route.spec.js
@@ -1,17 +1,17 @@
 const request = require('supertest');
 const api = require('../../index');
 
-describe('La ruta /products del ejercicio 1', () => {  
+describe('La ruta /currencies', () => {  
     let baseUrl = `/api/currencies?domain_override=mercadolibre.com.ar`;
 
-    it('1) Responde con un objeto que contiene con la propiedad currencies', async () => {
+    it('1) Responde con un objeto que contiene la propiedad `currencies`', async () => {
         const response = await request(api.app).get(`${baseUrl}`);
         let currencies = await JSON.parse(response.res.text);
 
         expect(Object.keys(currencies)).toContain('currencies');
     });
 
-    it('2) La propiedad currencies contiene las divisas requeridas', async () => {
+    it('2) La propiedad `currencies` contiene las divisas requeridas', async () => {
         const response = await request(api.app).get(`${baseUrl}`);
         let currencies = await JSON.parse(response.res.text);
         
